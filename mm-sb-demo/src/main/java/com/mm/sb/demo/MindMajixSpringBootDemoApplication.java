efe6335851dcab08e893ada9a958c9c1991dc85c
Sekhar Babu Patnam <35616006+sekharbabu4patnam@users.noreply.github.com>
2022-04-26 19:03:35 +0530

    Update MindMajixSpringBootDemoApplication.java

--- mm-sb-demo/src/main/java/com/mm/sb/demo/MindMajixSpringBootDemoApplication.java
+++ mm-sb-demo/src/main/java/com/mm/sb/demo/MindMajixSpringBootDemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MindMajixSpringBootDemoApplication {
+public class MindMajixSpringBootDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MindMajixSpringBootDemoApplication.class, args);
